460012475ce00a5aec4bdde0482caaec3537228e
Doyal1234 <137032190+Doyal1234@users.noreply.github.com>
2024-03-20 23:32:07 +0530

    Add files via upload

---
+++ second.js
@@ -0,0 +1,12 @@
+name=localStorage.getItem("name");
+document.getElementById("name").innerHTML=name;
+
+grade=localStorage.getItem("grade");
+document.getElementById("grade").innerHTML=grade;
+
+roll=localStorage.getItem("roll");
+document.getElementById("roll").innerHTML=roll;
+
+sec=localStorage.getItem("sec");
+document.getElementById("sec").innerHTML=sec;
+
